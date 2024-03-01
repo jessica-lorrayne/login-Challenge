dd4a53319f920c8bf6b60b012dcb99010c41578c
jessica <jessi.niza3@gmail.com>
2024-03-01 17:03:29 -0400

    correção na semantica

--- main.js
+++ main.js
@@ -1,5 +1,17 @@
 const email = document.getElementById('email')
 const errorMessage = document.getElementById('textEmail')
 const button = document.getElementById('button')
+const form = document.querySelector('.two')
+
+form.addEventListener('submit', (e) => {
+    console.log(email.value)
+    e.preventDefault()
+})
+
+// button.addEventListener()
 
 
+// console.log(email)
+// console.log(errorMessage)
+// console.log(button)
+// console.log(form)
