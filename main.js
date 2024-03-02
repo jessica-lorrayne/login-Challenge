76e3f7f0d33e382d7a7b4aa00721d8e2d861095c
jessica <jessi.niza3@gmail.com>
2024-03-02 17:34:13 -0400

    aplicando sass

--- main.js
+++ main.js
@@ -1,12 +1,21 @@
 const email = document.getElementById('email')
 const errorMessage = document.getElementById('textEmail')
 const button = document.getElementById('button')
-const form = document.querySelector('.two')
+const form = document.querySelector('form')
 
 form.addEventListener('submit', (e) => {
+    if(email.value ==''){
+        errorMessage.textContent = 'preencha o campo acima'
+    }
     console.log(email.value)
     e.preventDefault()
 })
+
+if (email.value == '@gmail.com') {
+    console.log("aprovado")
+} else {
+    console.log("nao aprovado")
+}
 
 // button.addEventListener()
 
