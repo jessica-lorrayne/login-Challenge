9524c0afe4b19f3cb1137e01a5624b88f236949e
jessica <jessi.niza3@gmail.com>
2024-03-05 00:56:30 -0400

    Projeto Finalizado

--- main.js
+++ main.js
@@ -1,49 +1,57 @@
+const form = document.querySelector('form');
+const bodyBlur = document.querySelector('body')
 const email = document.getElementById('email');
 const errorMessage = document.getElementById('textEmail');
-const button = document.getElementById('button');
 const invalidEmail = document.getElementById('textError');
-const form = document.querySelector('form');
-
+const button = document.getElementById('button');
+const modal = document.getElementById('modal');
+const buttonModal = document.getElementById('button_24')
+const blurBody = document.getElementById('blur')
 
 
 form.addEventListener('submit', (e) => {
-    if (email.value == '') {
+    if (email.value.trim() === '') {
         errorMessage.textContent = 'Preencha o campo acima';
+        
     } else {
         console.log(email.value);
-        
     }
     e.preventDefault();
 });
 
-email.addEventListener("keyup", () => {
-    if (validationEmail(email.value) !== true) {
+email.addEventListener("input", () => {
+    if (!validationEmail(email.value)) {
         invalidEmail.textContent = `O formato corresponde a ex: "teste@gmail.com"`;
     } else {
         invalidEmail.textContent = "";
     }
 });
 
+button.addEventListener('click', () => {
+    addOrRemoveClass();
+});
+
+function addOrRemoveClass() {
+    if (validationEmail(email.value)) {
+        email.classList.add('form_input');
+        email.classList.remove('error');
+        modal.style.display = 'block'
+        blurBody.style.display = 'block'
+    } else {
+        email.classList.add('error');
+        email.classList.remove('form_input');
+    }
+}
+
 function validationEmail(emailValue) {
     let emailPattern = /^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$/;
     return emailPattern.test(emailValue);
 }
 
-button.addEventListener('click', () => {
-    addOrRemoveClass();
-    return;
+buttonModal.addEventListener('click', () => {
+    modal.style.display = 'none';
+    blurBody.style.display = 'none';
 });
-
-function addOrRemoveClass() {
-    if (email.classList.contains('form_input') && !validationEmail(email.value)) {
-        email.classList.add('error');
-        email.classList.remove('form_input');
-    } 
-    else {
-        email.classList.remove('error');
-        email.classList.add('form_input');
-    }
-}
 
 // button.addEventListener()
 
